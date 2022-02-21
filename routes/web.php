2922439dccc997934e9c7023165782e0fe4456f5
Shamil <shamil@MacBook-Air-Shamil.local>
2022-02-21 21:49:11 +0300

    Выручай

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,18 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/', [MainController::class, 'home']);
+Route::get('/', [MainController::class, 'home'])->name('home');
 Route::get('/product', [MainController::class, 'product']);
 
 Route::get('/admin_panel', [AdminController::class, 'admin_panel']);
 
-Route::get('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'register']);
+Route::middleware('auth')->group(function () {
+    Route::get('/exit', [AuthController::class, 'exit']);
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'login_process']);
+    Route::get('/register', [AuthController::class, 'register']);
+    Route::post('/register', [AuthController::class, 'register_process']);
+});
